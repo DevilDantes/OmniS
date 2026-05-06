97c8080e04148552b2afe8c0061b862d7288d39f
Juan David Pedraza Valencia <160239574+DevilDantes@users.noreply.github.com>
2026-05-06 15:17:17 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -45,6 +45,7 @@ app.get('/api/health', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-  console.log('Servidor en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });

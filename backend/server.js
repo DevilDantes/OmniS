9bec980ae6fc8edb32c53b5b9e1f790dfe9f35ec
Juan David Pedraza Valencia <160239574+DevilDantes@users.noreply.github.com>
2026-05-12 12:06:28 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ import usuarios from '../routes/usuarios.js';
 import clientes from '../routes/clientes.js';
 import auditoria from '../routes/auditoria.js';
 import webhook from '../routes/webhook.js';
+import ai from '../routes/ia.js'
 
 // Configuración necesaria para usar "path" y "__dirname" en módulos ES6 (import)
 const __filename = fileURLToPath(import.meta.url);
@@ -45,6 +46,8 @@ app.use('/api/usuarios', usuarios);
 app.use('/api/clientes', clientes);
 app.use('/api/auditoria', auditoria);
 app.use('/api/webhook', webhook);
+app.use('/api/ia', ia);
+
 
 
 
